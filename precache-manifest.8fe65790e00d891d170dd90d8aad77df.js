b2b88912ab4f245d05eb1d944556b3d9f2fed529
JessDivyan <jessdivyan96@gmail.com>
2020-09-29 16:14:30 +0530

    Updates

---
+++ precache-manifest.8fe65790e00d891d170dd90d8aad77df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43c5b9fe8a877c1d1863714c9812117",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "479d7676f1424bbcab28",
+    "url": "/hangman/static/css/main.173ea76b.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479d7676f1424bbcab28",
+    "url": "/hangman/static/js/main.c26da65b.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
